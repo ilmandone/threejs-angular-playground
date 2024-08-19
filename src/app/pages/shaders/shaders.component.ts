8e4a9eec3220f16a2b8b245f1f6823f4a49e80a8
Andrea Mandini <a.mandini@vargroup.it>
2024-08-19 11:19:13 +0200

    feat(shaders): component setup

--- src/app/pages/shaders/shaders.component.ts
+++ src/app/pages/shaders/shaders.component.ts
@@ -1,14 +1,122 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  viewChild,
+} from '@angular/core';
+import {
+  AmbientLight,
+  DirectionalLight,
+  IcosahedronGeometry,
+  Mesh,
+  PCFShadowMap,
+  PerspectiveCamera,
+  Scene,
+  ShaderMaterial,
+  WebGLRenderer
+} from 'three';
+import {
+  OrbitControls
+} from 'three/examples/jsm/Addons.js';
+
+import vertexShader from './glsl/vertex.glsl';
+import fragmentShader from './glsl/fragment.glsl';
 
 @Component({
   selector: 'app-shaders',
   standalone: true,
   imports: [],
   templateUrl: './shaders.component.html',
-  styleUrl: './shaders.component.scss'
+  styleUrl: './shaders.component.scss',
 })
-class ShadersComponent {
+class ShadersComponent implements OnInit {
+  private _scene!: Scene;
+  private _camera!: PerspectiveCamera;
+  private _controls!: OrbitControls;
+  private _renderer!: WebGLRenderer;
 
+  private _threeEl = viewChild.required('three', { read: ElementRef });
+
+  ngOnInit(): void {
+    this._init();
+
+    this._windowResize();
+    this._renderer.setAnimationLoop(this._animate.bind(this));
+
+    this._threeEl().nativeElement.appendChild(this._renderer.domElement);
+  }
+
+  //#region privates
+
+  private _basic() {
+    const _scene = new Scene();
+    const _camera = new PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      100
+    );
+    _camera.position.z = -2
+
+    const _renderer = new WebGLRenderer({ antialias: true });
+    _renderer.shadowMap.enabled = true;
+    _renderer.shadowMap.type = PCFShadowMap;
+    _renderer.setPixelRatio(window.devicePixelRatio);
+
+    return { _scene, _camera, _renderer };
+  }
+
+  private _addLights(scene: Scene) {
+    // lighting
+    const dirLight = new DirectionalLight('#ffffff', 0.75);
+    dirLight.position.set(5, 5, 5);
+    scene.add(dirLight)
+
+    const ambientLight = new AmbientLight('#ffffff', 0.2);
+    scene.add(dirLight, ambientLight);
+  }
+
+  private _buildScene(scene: Scene) {
+    const geometry = new IcosahedronGeometry(1, 5);
+    const material = new ShaderMaterial({
+      vertexShader: vertexShader as string,
+      fragmentShader: fragmentShader as string,
+    });
+
+    const ico = new Mesh(geometry, material);
+    scene.add(ico);
+  }
+
+  private _init() {
+    Object.assign(this, this._basic());
+
+    this._addLights(this._scene);
+    this._buildScene(this._scene)
+
+    this._controls = new OrbitControls(
+      this._camera,
+      this._threeEl().nativeElement
+    );
+  }
+
+  //#edndregion
+
+  //#region Runtime
+
+  private _animate() {
+    this._controls.update();
+    this._renderer.render(this._scene, this._camera);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  private _windowResize() {
+    this._renderer.setSize(window.innerWidth, window.innerHeight);
+    this._camera.aspect = window.innerWidth / window.innerHeight;
+    this._camera.updateProjectionMatrix();
+  }
+
+  //#endregion
 }
 
-export default ShadersComponent
+export default ShadersComponent;
